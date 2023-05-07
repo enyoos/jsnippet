3cfafdd373ada1b79b554c85310e9ca4eadb2286
enyoos <lebedevhh@outlook.fr>
2023-05-07 18:59:23 -0400

    *

--- javaPatterns/Singleton/Singleton.java
+++ javaPatterns/Singleton/Singleton.java
@@ -1,10 +1,14 @@
 public final class Student{
-    private volatile static Student student = new Student();
+    private volatile static Student student;
     private Student(){}
     public static Student getStudendInstance(){
-        if (student == null){
-           return new Student(); 
+        if (student==null){
+            synchronized(Student.class){
+                if (student == null){
+                    return new Student(); 
+                }
+            }
         }
-        return null;
+       return student;
     }
 }
